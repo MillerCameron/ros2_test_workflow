5b14eae9217d7cc115f6c81ae03aded32b0e3eac
spaceboi153 <208745836+MillerCameron@users.noreply.github.com>
2025-05-27 16:30:57 -0600

    time to break

--- packages/cpp_pubsub/src/subscriber_member_function.cpp
+++ packages/cpp_pubsub/src/subscriber_member_function.cpp
@@ -15,7 +15,7 @@ class MinimalSubscriber : public rclcpp::Node
     }
 
   private:
-    void topic_callback(const std_msgs::msg::String & msg) const
+//    void topic_callback(const std_msgs::msg::String & msg) const
     {
       RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
     }
