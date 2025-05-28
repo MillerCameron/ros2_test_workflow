221f06fad85ff90dcc56f1a7f9a3eae9eb53f5a3
MillerCameron <millercameronj@gmail.com>
2025-05-28 10:11:52 -0600

    Update publisher_member_function.cpp

--- packages/cpp_pubsub/src/publisher_member_function.cpp
+++ packages/cpp_pubsub/src/publisher_member_function.cpp
@@ -13,7 +13,7 @@ using namespace std::chrono_literals;
 
 class MinimalPublisher : public rclcpp::Node
 {
-  public:
+  public:324
     MinimalPublisher()
     : Node("minimal_publisher"), count_(0)
     {
